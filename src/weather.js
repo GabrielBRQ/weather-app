812cf2b2cdfaf74f250d699c1ebfdb4c1483e86e
GabrielBRQ <gabrielmedvalle@gmail.com>
2023-10-27 11:03:07 -0300

    change background daytime

--- src/weather.js
+++ src/weather.js
@@ -1,20 +1,23 @@
 import _, { get } from 'lodash';
+import searchIcon from './img/search.png'
 import './style.css';
 
 const celsiusTitle = document.querySelector(".celsius .title");
 const celsiusTemp = document.querySelector(".celsius .temp");
 const fahrenheitTitle = document.querySelector(".fahrenheit .title");
 const fahrenheitTemp = document.querySelector(".fahrenheit .temp");
-const searchInput = document.querySelector('input');
-const button = document.querySelector('button');
-const imgButton = document.querySelector('button img');
+const searchInput = document.querySelector("input");
+const button = document.querySelector("button");
+const imgButton = document.querySelector("button img");
 const tempTitle = document.querySelector("p");
+
 let temperatureCelsius;
 let temperatureFahrenheit;
 let city;
 let country;
+let dayTime;
 
-async function getTemp(){
+async function getInfo(){
    const searchTerm = searchInput.value.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
    if(searchTerm !== ''){
       const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=c2ee4386538f4a81883124521232510&q=${searchTerm}&aqi=no`, { mode: 'cors' })
@@ -23,6 +26,9 @@ async function getTemp(){
       temperatureFahrenheit = data.current.temp_f;
       city = data.location.name;
       country = data.location.country;
+      const time = data.location.localtime.split(' ');
+      dayTime = time[1]
+      changeDayStyle();
       setText();
    }
 }
@@ -35,12 +41,42 @@ function setText(){
    tempTitle.textContent = `Temperature in ${city}, ${country}`;
 }
 
-button.addEventListener('click', getTemp);
+function changeDayStyle(){
+   const moonLayout = document.querySelector(".moon-layout")
+   const body = document.body;
+   const moon = document.querySelector(".moon");
+   const moonWidth = moon.offsetWidth;
+   const stars = document.querySelector(".stars");
+   const timePart = dayTime.split(':');
+   const hour = parseInt(timePart[0]);
+   moonLayout.style.opacity = '1';
+   if(hour < 16){
+      moon.style.backgroundColor = "#f7f4e0"
+      moon.style.right = '80vw';
+      stars.style.opacity = '0';
+      moon.style.opacity = "1";
+      body.style.backgroundColor = '#7FC3FF';
+   }else if(hour < 19){
+      moon.style.backgroundColor = "#f7f4e0"
+      moon.style.right = `calc(50vw - ${moonWidth}px)`;
+      moon.style.opacity = "0.5";
+      stars.style.opacity = '0';
+      body.style.backgroundColor = '#FDA92C';
+   }else{
+      moon.style.backgroundColor = "white"
+      moon.style.right = '10vw';
+      moon.style.opacity = "1";
+      stars.style.opacity = '1';
+   }
+   console.log(hour);
+}
+
+button.addEventListener('click', getInfo);
    searchInput.addEventListener('keyup', function(event) {
       if (event.key === 'Enter') {
-         getTemp();
+         getInfo();
       }
    }
 );
 
-imgButton.src = "/dist/297d05ac11b74e171a85.png";
+imgButton.src = searchIcon
